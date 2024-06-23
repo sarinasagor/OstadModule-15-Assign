36bb11317b8bc448bd88714579b1c755a6841dcc
sarinasagor <sarinasagor@outlook.com>
2024-06-24 01:47:59 +0600

    initial

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+use App\Http\Controllers\ProfileController;
+
+Route::get('/profile/{id}', [ProfileController::class, 'index']);
